259d1333e8a13d9156b387d026196b868f0b939b
Mateus Santana <mateus-sgn@hotmail.com>
2024-05-03 09:46:54 -0300

    feat: Start repasse project

---
+++ repasse/src/main/java/com/repasse/RepasseApplication.java
@@ -0,0 +1,13 @@
+package com.repasse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepasseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepasseApplication.class, args);
+	}
+
+}
